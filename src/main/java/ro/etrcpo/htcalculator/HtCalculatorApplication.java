334c8d5d54486e03c7519792e60a75217e1dc522
Emanuel Trandafir <emanuel.trandafir@pss.ro>
2020-11-29 22:21:03 +0200

    uploaded empty spring boot project

---
+++ src/main/java/ro/etrcpo/htcalculator/HtCalculatorApplication.java
@@ -0,0 +1,23 @@
+package ro.etrcpo.htcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class HtCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtCalculatorApplication.class, args);
+	}
+
+}
+
+@RestController
+class TestController {
+	@GetMapping("/isAlive")
+	public String test() {
+		return "hattrick rating contribution up and running!";
+	}
+}
